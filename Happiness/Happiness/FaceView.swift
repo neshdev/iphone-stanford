299904949a97f5b831471c344d9356abb65576bf
nesh <dhineshdevanathan@gmail.com>
2016-05-02 01:42:35 -0400

    finished watching lecture 5

---
+++ Happiness/Happiness/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by Dhinesh Kumar Devanathan on 5/2/16.
+//  Copyright (c) 2016 NeshDev Studios. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ lecture5a/Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Dhinesh Kumar Devanathan on 5/2/16.
+//  Copyright (c) 2016 NeshDev Studios. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
