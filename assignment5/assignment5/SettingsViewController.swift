650006a0ccb17e531457651b429d901a92b8c83f
nesh <dhineshdevanathan@gmail.com>
2016-06-02 20:57:52 -0400

    added tab vc

---
+++ assignment5/assignment5/SettingsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsViewController.swift
+//  assignment5
+//
+//  Created by Dhinesh Kumar Devanathan on 6/2/16.
+//  Copyright © 2016 NeshDev Studios. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
